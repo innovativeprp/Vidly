7f6f60c53c44520d9d65f30ca81520a56abf4553
Praveen Raveendran <Praveen.Raveendran@hotmail.com>
2018-06-25 22:11:11 +0530

    Added MembershipTypeName

---
+++ Vidly/Migrations/201806251621587_AddMemebershipNameData.cs
@@ -0,0 +1,21 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMemebershipNameData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Delete from MembershipTypes");
+            Sql("Insert into MembershipTypes (Id,SignUpFee,DurationByMonth,Discount,Name) values(1,0,0,0,'Pay As You Go')");
+            Sql("Insert into MembershipTypes (Id,SignUpFee,DurationByMonth,Discount,Name) values(2,30,1,10,'Monthly')");
+            Sql("Insert into MembershipTypes (Id,SignUpFee,DurationByMonth,Discount,Name) values(3,90,3,20,'Quarterly')");
+            Sql("Insert into MembershipTypes (Id,SignUpFee,DurationByMonth,Discount,Name) values(4,300,12,30,'Annualy')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
